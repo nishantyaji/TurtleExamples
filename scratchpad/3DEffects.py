97c75747adea11e76d9c8a5e0190ea4eaadcc16d
nishantyaji <nishant.yaji@gmail.com>
2019-01-20 19:00:21 +0530

    Refactoring

--- scratchpad/3DEffects.py
+++ scratchpad/3DEffects.py
@@ -21,10 +21,11 @@ def drawRectangle(aame, coordinates, heading, height, width, fillColor, bool3D):
     aame.forward(height)
     aame.end_fill()
 
-    step = 5
     if bool3D:
+        minDistance = min(255-fillColor[0], 255-fillColor[1], 255-fillColor[2])
+        widthSteps = (width//4)+1
+        step = minDistance // widthSteps
         while width > 3:
-            print(width)
             coordinates = (coordinates[0]+2, coordinates[1]+2)
             height = height - 4
             width = width - 4
@@ -37,11 +38,12 @@ def lighterShade(rgb,step=1):
 
 def display():
     t = turtle.Pen()
+    t.speed(0)
     coordinates = -200, -200
     resetOrigin(t,coordinates)
     heading = t.heading()
-    height = 70
-    width = 30
+    height = 270
+    width = 130
     screen = t.getscreen()
     screen.bgcolor('black')
     screen.colormode(255)
